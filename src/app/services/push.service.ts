30e544e3862534855b32855ccb60e1ad71823595
VictorManuelSalas <victorsalasnz@gmail.com>
2022-12-21 20:42:43 -0600

    adelanto

--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -6,18 +6,18 @@ import OneSignal from 'onesignal-cordova-plugin';
 })
 export class PushService {
   //Objetos de mensajes
-  mensajes: any[] = [
+  public mensajes: any[] = [
     {
       title: 'Titulo de la notificacion PUSH',
       body: 'Este es el body de la notificacion push',
       date: new Date()
     }
   ]
-
+  
   id_onesignal = '8d1620a0-bc08-4ab2-9a79-3ffb5aee82a6';
   constructor() { }
 
-  
+
   // Call this function when your app starts
   OneSignalInit() {
     // Uncomment to set OneSignal device logging to VERBOSE  
@@ -25,34 +25,41 @@ export class PushService {
 
     // NOTE: Update the setAppId value below with your OneSignal AppId.
     OneSignal.setAppId(this.id_onesignal);
+    OneSignal.setNotificationWillShowInForegroundHandler(function(jsonData){
+      alert('info: ' + jsonData);
+    })
     OneSignal.setNotificationOpenedHandler(function (jsonData) {
       let data = JSON.stringify(jsonData);
-      console.log('notificationOpenedCallback: ' + data);
-    
+      alert('notificationOpenedCallback: ' + data);
+      return data;
     });
-
+ 
     // Prompts the user for notification permissions.
     //    * Since this shows a generic native prompt, we recommend instead using an In-App Message to prompt for notification permission (See step 7) to better communicate to your users what notifications they will get.
     OneSignal.promptForPushNotificationsWithUserResponse(function (accepted) {
-      console.log("User accepted notifications: " + accepted);
+      alert("User accepted notifications: " + accepted);
     });
 
 
-  }
 
-  
-  async notificacionRecibida(data: any) {
-    const payload = data.payload;
-
-    const existPush = this.mensajes.find(mensaje => mensaje.notificacionID = payload.notificacionID)
-
-    if (existPush){
-      return;
-    }
-
-    this.mensajes.unshift(payload);
   }
 
 
 }
+
+/*
+async notificacionRecibida(data: any) {
+  const payload = data.payload;
+
+  const existPush = this.mensajes.find(mensaje => mensaje.notificacionID = payload.notificacionID)
+
+  if (existPush){
+    return;
+  }
+
+  this.mensajes.unshift(payload);
+}*/
+
+
+
 
