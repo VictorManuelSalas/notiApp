0855e740532716c699bb8c71d947e04ab17f1b4e
VictorManuelSalas <victorsalasnz@gmail.com>
2023-01-05 21:13:56 -0600

    adelanto

--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -8,57 +8,71 @@ export class PushService {
   //Objetos de mensajes
   public mensajes: any[] = [
     {
-      title: 'Titulo de la notificacion PUSH',
-      body: 'Este es el body de la notificacion push',
-      date: new Date()
+      //title: 'Titulo de la notificacion PUSH',
+      // body: 'Este es el body de la notificacion push',
+      //date: new Date()
     }
   ]
-  
+
   id_onesignal = '8d1620a0-bc08-4ab2-9a79-3ffb5aee82a6';
-  constructor() { }
+  constructor() {
 
+  }
 
+  //data = '';
   // Call this function when your app starts
-  OneSignalInit() {
+  OneSignalInit(): void {
     // Uncomment to set OneSignal device logging to VERBOSE  
     // OneSignal.setLogLevel(6, 0);
-
     // NOTE: Update the setAppId value below with your OneSignal AppId.
     OneSignal.setAppId(this.id_onesignal);
-    OneSignal.setNotificationWillShowInForegroundHandler(function(jsonData){
-      alert('info: ' + jsonData);
-    })
-    OneSignal.setNotificationOpenedHandler(function (jsonData) {
+
+    OneSignal.setNotificationOpenedHandler((jsonData) => {
       let data = JSON.stringify(jsonData);
       alert('notificationOpenedCallback: ' + data);
-      return data;
+
     });
- 
+
+
+    OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent) => {
+      alert('OneSignal: notification will show in foreground:' + notificationReceivedEvent)
+
+      const notification = notificationReceivedEvent.getNotification()
+      notificationReceivedEvent.complete(notification);
+      this.notificacionRecibida(notification);
+    });
+
     // Prompts the user for notification permissions.
     //    * Since this shows a generic native prompt, we recommend instead using an In-App Message to prompt for notification permission (See step 7) to better communicate to your users what notifications they will get.
     OneSignal.promptForPushNotificationsWithUserResponse(function (accepted) {
       alert("User accepted notifications: " + accepted);
     });
-
-
-
   }
 
+  infoLocal: any;
+  informacion(data: any) {
+    alert('Datos de la funcion creada: ' + data);
 
+
+    this.mensajes.unshift(data)
+    localStorage.setItem('noti', 'info');
+    this.infoLocal = localStorage.getItem('noti');
+  }
+
+  async notificacionRecibida(data: any) {
+    const payload = data.rawPayload;
+
+    alert('Payloas' + payload)
+
+    const existPush = this.mensajes.find(mensaje => mensaje.notificacionID = payload.notificacionID)
+
+    if (existPush) {
+      return;
+    }
+
+    this.mensajes.unshift(payload);
+  }
 }
-
-/*
-async notificacionRecibida(data: any) {
-  const payload = data.payload;
-
-  const existPush = this.mensajes.find(mensaje => mensaje.notificacionID = payload.notificacionID)
-
-  if (existPush){
-    return;
-  }
-
-  this.mensajes.unshift(payload);
-}*/
 
 
 
